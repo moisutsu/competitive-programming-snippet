6d788480ddcbdd1296a7c71c603aad30bc02e8c4
moisutsu <moisutsu@gmail.com>
2020-10-01 11:11:47 +0900

    Add factorial

--- src/math.rs
+++ src/math.rs
@@ -25,6 +25,11 @@ where
     a * b / gcd(a, b)
 }
 
+#[snippet(name = "@factorial")]
+fn factorial(n: usize) -> usize {
+    (1..=n).fold(1, |sigma, i| sigma * i)
+}
+
 #[test]
 fn test_gcd() {
     assert_eq!(gcd(5i128, 5i128), 5i128);
@@ -38,4 +43,12 @@ fn test_lcm() {
     assert_eq!(lcm(10, 5), 10);
     assert_eq!(lcm(7usize, 11usize), 77usize);
     assert_eq!(lcm(1i128, 1i128), 1i128);
+}
+
+#[test]
+fn test_factorial() {
+    assert_eq!(factorial(0), 1);
+    assert_eq!(factorial(1), 1);
+    assert_eq!(factorial(4), 24);
+    assert_eq!(factorial(6), 720);
 }
