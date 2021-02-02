3f8adbe992538efa8dc3c3e8ac07b39fec070f4d
moisutsu <moisutsu@gmail.com>
2021-02-02 15:28:00 +0900

    Change YesNo type macro -> function

--- src/yes_no.rs
+++ src/yes_no.rs
@@ -1,31 +1,27 @@
 use cargo_snippet::snippet;
 
 #[snippet("@YesNo")]
-macro_rules! YesNo {
-    ($flag:expr) => {
-        if $flag {
-            "Yes"
-        } else {
-            "No"
-        }
-    };
+fn YesNo(flag: bool) -> &'static str {
+    if flag {
+        "Yes"
+    } else {
+        "No"
+    }
 }
 
 #[snippet("@YESNO")]
-macro_rules! YESNO {
-    ($flag:expr) => {
-        if $flag {
-            "YES"
-        } else {
-            "NO"
-        }
-    };
+fn YESNO(flag: bool) -> &'static str {
+    if flag {
+        "YES"
+    } else {
+        "NO"
+    }
 }
 
 #[test]
 fn test_yesno() {
-    assert_eq!(YESNO!(true), "YES");
-    assert_eq!(YESNO!(false), "NO");
-    assert_eq!(YesNo!(true), "Yes");
-    assert_eq!(YesNo!(false), "No");
+    assert_eq!(YESNO(true), "YES");
+    assert_eq!(YESNO(false), "NO");
+    assert_eq!(YesNo(true), "Yes");
+    assert_eq!(YesNo(false), "No");
 }
