e6655366d30556239724e2b1eb3879642dae72c5
moisutsu <moisutsu@gmail.com>
2021-02-02 15:35:57 +0900

    Revert "Change YesNo type macro -> function"
    This reverts commit 3f8adbe992538efa8dc3c3e8ac07b39fec070f4d.

--- src/yes_no.rs
+++ src/yes_no.rs
@@ -1,27 +1,31 @@
 use cargo_snippet::snippet;
 
 #[snippet("@YesNo")]
-fn YesNo(flag: bool) -> &'static str {
-    if flag {
-        "Yes"
-    } else {
-        "No"
-    }
+macro_rules! YesNo {
+    ($flag:expr) => {
+        if $flag {
+            "Yes"
+        } else {
+            "No"
+        }
+    };
 }
 
 #[snippet("@YESNO")]
-fn YESNO(flag: bool) -> &'static str {
-    if flag {
-        "YES"
-    } else {
-        "NO"
-    }
+macro_rules! YESNO {
+    ($flag:expr) => {
+        if $flag {
+            "YES"
+        } else {
+            "NO"
+        }
+    };
 }
 
 #[test]
 fn test_yesno() {
-    assert_eq!(YESNO(true), "YES");
-    assert_eq!(YESNO(false), "NO");
-    assert_eq!(YesNo(true), "Yes");
-    assert_eq!(YesNo(false), "No");
+    assert_eq!(YESNO!(true), "YES");
+    assert_eq!(YESNO!(false), "NO");
+    assert_eq!(YesNo!(true), "Yes");
+    assert_eq!(YesNo!(false), "No");
 }
