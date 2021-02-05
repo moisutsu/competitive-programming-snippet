07d14f92183ee06a561e5d8b4a8c1561bffeff2a
moisutsu <moisutsu@gmail.com>
2021-02-05 22:41:13 +0900

    Use associated types for binary search

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,14 +1,16 @@
 use cargo_snippet::snippet;
 
 #[snippet("@binary_search")]
-pub trait BinarySearch<T> {
-    fn lower_bound(&self, x: T) -> usize;
-    fn upper_bound(&self, x: T) -> usize;
+pub trait BinarySearch {
+    type Element;
+    fn lower_bound(&self, x: &Self::Element) -> usize;
+    fn upper_bound(&self, x: &Self::Element) -> usize;
 }
 
 #[snippet("@binary_search")]
-impl<T: Ord> BinarySearch<T> for [T] {
-    fn lower_bound(&self, x: T) -> usize {
+impl<T: Ord> BinarySearch for [T] {
+    type Element = T;
+    fn lower_bound(&self, x: &Self::Element) -> usize {
         let mut low = 0;
         let mut high = self.len();
 
@@ -26,7 +28,7 @@ impl<T: Ord> BinarySearch<T> for [T] {
         low
     }
 
-    fn upper_bound(&self, x: T) -> usize {
+    fn upper_bound(&self, x: &Self::Element) -> usize {
         let mut low = 0;
         let mut high = self.len();
 
@@ -48,7 +50,19 @@ impl<T: Ord> BinarySearch<T> for [T] {
 #[test]
 fn test_binary_search() {
     let vec = vec![1, 2, 4, 4, 6, 7, 12, 54, 60];
+    // lower_bound
+    assert_eq!(vec.lower_bound(&0), 0);
+    assert_eq!(vec.lower_bound(&1), 0);
+    assert_eq!(vec.lower_bound(&4), 2);
+    assert_eq!(vec.lower_bound(&5), 4);
+    assert_eq!(vec.lower_bound(&60), 8);
+    assert_eq!(vec.lower_bound(&100), 9);
 
-    assert_eq!(vec.lower_bound(4), 2);
-    assert_eq!(vec.upper_bound(4), 4);
+    // upper_bound
+    assert_eq!(vec.upper_bound(&0), 0);
+    assert_eq!(vec.upper_bound(&1), 1);
+    assert_eq!(vec.upper_bound(&4), 4);
+    assert_eq!(vec.upper_bound(&5), 4);
+    assert_eq!(vec.upper_bound(&60), 9);
+    assert_eq!(vec.upper_bound(&100), 9);
 }
