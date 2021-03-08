5a4f61fa654cd86389d217d4f6e6715bd4afedd4
moisutsu <moisutsu@gmail.com>
2021-03-08 20:22:17 +0900

    Rename const name MOVE -> ADJ

--- src/consts.rs
+++ src/consts.rs
@@ -1,10 +1,10 @@
 use cargo_snippet::snippet;
 
-#[snippet("@move_4")]
-const MOVE_4: [(usize, usize); 4] = [(1, 0), (0, 1), (!0, 0), (0, !0)];
+#[snippet("@adj_4")]
+const ADJ_4: [(usize, usize); 4] = [(1, 0), (0, 1), (!0, 0), (0, !0)];
 
-#[snippet("@move_8")]
-const MOVE_8: [(usize, usize); 8] = [
+#[snippet("@adj_8")]
+const ADJ_8: [(usize, usize); 8] = [
     (1, 0),
     (0, 1),
     (!0, 0),
