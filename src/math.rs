b08dc144799d22e08156c0a98dfc98bf65c357bc
moisutsu <moisutsu@gmail.com>
2021-03-19 09:55:00 +0900

    Add to_base

--- src/math.rs
+++ src/math.rs
@@ -88,6 +88,41 @@ macro_rules! mod_inv {
     }};
 }
 
+#[snippet("@to_base")]
+trait ToBase<T> {
+    fn to_base(self, base: T) -> T;
+}
+
+#[snippet("@to_base")]
+impl<T> ToBase<T> for T
+where
+    T: Copy
+        + std::ops::Add<Output = T>
+        + std::ops::Sub<Output = T>
+        + std::ops::Mul<Output = T>
+        + std::ops::Div<Output = T>
+        + std::ops::Rem<Output = T>
+        + std::cmp::PartialOrd,
+{
+    fn to_base(self, base: T) -> T {
+        let zero = base - base;
+        let one = base / base;
+        let ten = (one + one + one) * (one + one + one) + one;
+
+        let mut n = self;
+        let mut ret = zero;
+        let mut digit = one;
+
+        while n > zero {
+            ret = ret + n % base * digit;
+            n = n / base;
+            digit = digit * ten;
+        }
+
+        ret
+    }
+}
+
 #[test]
 fn test_gcd() {
     assert_eq!(gcd(5i128, 5i128), 5i128);
@@ -135,4 +170,16 @@ fn test_mod_inv() {
     let b = 100000;
     a %= MOD;
     assert_eq!(a * mod_inv!(b, MOD) % MOD, 123456789);
+}
+
+#[test]
+fn test_to_base() {
+    let n = 100i128;
+    assert_eq!(n.to_base(2), 1100100);
+    assert_eq!(n.to_base(8), 144);
+    assert_eq!(n.to_base(16), 64);
+
+    let n = 492usize;
+    assert_eq!(n.to_base(2), 111101100);
+    assert_eq!(n.to_base(8), 754);
 }
